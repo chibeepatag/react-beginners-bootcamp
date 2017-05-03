6ca69271a63ba600b7de35e5fe623315723d5818
Celine Patag Miranda <celine.patag@gmail.com>
2017-05-03 12:24:20 +0800

    (module-1) step 7: hot reloading (Flip)

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,32 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+
 import FlipApp from './Flip'
 import GuessNumber from './GuessNumber'
 import FizzBuzz from './FizzBuzz'
 import Auth from './Auth'
 import Series from './Series'
 
-ReactDOM.render(
-  <FlipApp />,
-  document.getElementById('flip-app'),
-)
+const render = (AppComponent, appName) => {
+  ReactDOM.render(
+    <AppContainer>
+      <AppComponent />
+    </AppContainer>,
+    document.getElementById(`${appName}-app`),
+    )
+}
 
-ReactDOM.render(
-  <GuessNumber />,
-  document.getElementById('guessnumber-app'),
-)
+if (module.hot) {
+  module.hot.accept('./Flip', () => {
+    const NextFlipApp = require('./Flip').default // eslint-disable-line global-require
+    render(NextFlipApp, 'flip')
+  })
 
-ReactDOM.render(
-  <FizzBuzz />,
-  document.getElementById('fizzbuzz-app'),
-)
+  module.hot.accept('./GuessNumber', () => {
+    const NextGuessNumber = require('./GuessNumber').default // eslint-disable-line global-require
+    render(NextGuessNumber, 'guessnumber')
+  })
 
-ReactDOM.render(
-  <Auth />,
-  document.getElementById('auth-app'),
-)
+  module.hot.accept('./FizzBuzz', () => {
+    const NextFizzBuzz = require('./FizzBuzz').default // eslint-disable-line global-require
+    render(NextFizzBuzz, 'fizzbuzz')
+  })
 
-ReactDOM.render(
-  <Series />,
-  document.getElementById('series-app'),
-)
+  module.hot.accept('./Auth', () => {
+    const NextAuth = require('./Auth').default // eslint-disable-line global-require
+    render(NextAuth, 'auth')
+  })
+
+  module.hot.accept('./Series', () => {
+    const NextSeries = require('./Series').default // eslint-disable-line global-require
+    render(NextSeries, 'series')
+  })
+}
+
+
+render(FlipApp, 'flip');
+render(GuessNumber, 'guessnumber');
+render(FizzBuzz, 'fizzbuzz');
+render(Auth, 'auth');
+render(Series, 'series');
