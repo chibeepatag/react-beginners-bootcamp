5e2e6fc6dcb0478fc4fa456cc1cf98acb6f9e1c3
Celine Patag Miranda <celine.patag@gmail.com>
2017-05-03 15:31:08 +0800

    (module-1) step 12: class components and initial state (Rest)

--- src/client/Series.jsx
+++ src/client/Series.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 
-const Series = () =>
-  <div className="container app-container">
-    <h2>Series</h2>
-    <div className="row">
-      <div className="col-md-4">
-        <h3>Controls</h3>
-        <div className="form-group">
-          <input className="form-control" placeholder="Search Series" />
-        </div>
-        <div style={{ width: 100, border: 1, solid: '#eee', borderRadius: 4 }}>
-          <div style={{ padding: 5 }}>Suthpark</div>
-          <div style={{ padding: 5, background: '#eee' }}>Mr. Robot</div>
+class Series extends React.Component {
+  state = {
+    query: '',
+    selected: [],
+  }
+
+  render() {
+    return (
+      <div className="container app-container">
+        <h2>Series</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <h3>Controls</h3>
+            <div className="form-group">
+              <input className="form-control" value={this.state.query} placeholder="Search Series" />
+            </div>
+          </div>
+          <div className="col-md-2 col-md-offset-2">
+            <h3>Output</h3>
+            <div className="well" />
+          </div>
         </div>
       </div>
-      <div className="col-md-2 col-md-offset-2">
-        <h3>Output</h3>
-        <div className="well">
-          <div>Southpark</div>
-          <div>Mr. robot</div>
-          <div>Seinfeld</div>
-        </div>
-      </div>
-    </div>
-  </div>
+    )
+  }
+}
 export default Series
