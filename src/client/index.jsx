1cd8d102af7d1f362f8d61122f743dd53a350843
Celine Patag Miranda <celine.patag@gmail.com>
2017-05-03 11:14:20 +0800

    (module-1) step 3: first react component (Flip)

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const FlipApp = () => <h2>Flip!</h2>
 ReactDOM.render(
-  <h2>Flip</h2>,
+  <FlipApp />,
   document.getElementById('flip-app'),
+)
+
+ReactDOM.render(
+  <h2>guessnumber</h2>,
+  document.getElementById('guessnumber-app'),
+)
+
+ReactDOM.render(
+  <h2>fizzbuzz</h2>,
+  document.getElementById('fizzbuzz-app'),
+)
+
+ReactDOM.render(
+  <h2>auth</h2>,
+  document.getElementById('auth-app'),
+)
+
+ReactDOM.render(
+  <h2>series</h2>,
+  document.getElementById('series-app'),
 )
