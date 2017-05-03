e7989124183a596f5f615cefd765c7b6a22e1431
Celine Patag Miranda <celine.patag@gmail.com>
2017-05-03 11:32:42 +0800

    (module-1) step 5: separate app to own file (Flip)

---
+++ src/client/Flip.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const FlipApp = () => <h2>Flip! In its own component</h2>
+
+export default FlipApp
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import FlipApp from './Flip'
 
-const FlipApp = () => <h2>Flip!</h2>
 ReactDOM.render(
   <FlipApp />,
   document.getElementById('flip-app'),
@@ -9,24 +9,24 @@ ReactDOM.render(
 
 const GuessNumber = () => <h2>Guess Number</h2>
 ReactDOM.render(
-  <GuessNumber/>,
+  <GuessNumber />,
   document.getElementById('guessnumber-app'),
 )
 
 const FizzBuzz = () => <h2>Fizz Buzz</h2>
 ReactDOM.render(
-  <FizzBuzz/>,
+  <FizzBuzz />,
   document.getElementById('fizzbuzz-app'),
 )
 
 const Auth = () => <h2>Auth</h2>
 ReactDOM.render(
-  <Auth/>,
+  <Auth />,
   document.getElementById('auth-app'),
 )
 
 const Series = () => <h2>Series</h2>
 ReactDOM.render(
-  <Series/>,
+  <Series />,
   document.getElementById('series-app'),
 )
